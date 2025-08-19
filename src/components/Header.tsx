13e0364853ec3e72300cf02337a1a72943e58c09
Mahmood Ahmed <Mahmood.Ahmed.SE.@outlook.com>
2025-08-19 23:18:51 +0400

    feat: about us page and added header closing navbar by touching screen outside the navbar.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,73 +1,109 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { Sling as Hamburger } from "hamburger-react";
 
 import brandIcon from "/assets/brand-icon.webp";
 import { NavLink } from "react-router-dom";
 
-import './header.css';
+import "./header.css";
 
 export default function Header({ isStaticColor }: { isStaticColor: boolean }) {
-    const [isOpen, setOpen] = useState(false);
-    const [scrolled, setScrolled] = useState(false);
+	const [isOpen, setOpen] = useState(false);
+	const [scrolled, setScrolled] = useState(false);
 
-    if (!isStaticColor) {
-        useEffect(() => {
-            const onScroll = () => setScrolled(window.scrollY > 50);
-            window.addEventListener("scroll", onScroll);
-            return () => window.removeEventListener("scroll", onScroll);
-        }, []);
-    }
+	const menubarRef = useRef<HTMLElement>(null);
+	const toggleButtonRef = useRef<HTMLDivElement>(null);
 
-    return <header className={isStaticColor ? "header dark-bg" : scrolled ? "header dark-bg" : "header"}>
-        <div className="brand-icon">
-            <img src={brandIcon} />
-        </div>
+	useEffect(() => {
+		const handleMouseOut = (e: MouseEvent) => {
+			if (
+				menubarRef.current &&
+				!menubarRef.current.contains(e.target as Node) &&
+				toggleButtonRef.current &&
+				!toggleButtonRef.current.contains(e.target as Node)
+      ) {
+				setOpen(false);
+			}
+		};
+		window.addEventListener("mousedown", handleMouseOut);
 
-        <nav className={isOpen ? "menu open" : "menu"}>
-            <ul>
-                <li>
-                    <NavLink
-                        to="/"
-                        className={({ isActive, isPending }) =>
-                            isPending ? "pending" : isActive ? "active" : ""
-                        }
-                        onClick={() => setOpen(false)}
-                    >HOME</NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to="/about-us"
-                        className={({ isActive, isPending }) =>
-                            isPending ? "pending" : isActive ? "active" : ""
-                        }
-                        onClick={() => setOpen(false)}
-                    >ABOUT US</NavLink>
-                </li>
-                <li>
-                    <a
-                        href="/#testimonials"
-                        onClick={() => setOpen(false)}
-                    >TESTIMONIALS</a>
-                </li>
-                <li>
-                    <NavLink
-                        to="/contact-us"
-                        className={({ isActive, isPending }) =>
-                            isPending ? "pending" : isActive ? "active" : ""
-                        }
-                        onClick={() => setOpen(false)}
-                    >CONTACT</NavLink>
-                </li>
-            </ul>
-        </nav>
+		return () => window.removeEventListener("mousedown", handleMouseOut);
+	});
+	if (!isStaticColor) {
+		useEffect(() => {
+			const onScroll = () => setScrolled(window.scrollY > 50);
+			window.addEventListener("scroll", onScroll);
+			return () => window.removeEventListener("scroll", onScroll);
+		}, []);
+	}
 
-        <Hamburger
-            toggled={isOpen}
-            toggle={setOpen}
-            direction="right"
-            size={45}
-            color="white"
-        />
-    </header>
+	return (
+		<header
+			className={
+				isStaticColor
+					? "header dark-bg"
+					: scrolled
+						? "header dark-bg"
+						: "header"
+			}
+		>
+			<div className="brand-icon">
+				<img src={brandIcon} />
+			</div>
+
+			<nav className={isOpen ? "menu open" : "menu"} ref={menubarRef}>
+				<ul>
+					<li>
+						<NavLink
+							to="/"
+							className={({ isActive, isPending }) =>
+								isPending ? "pending" : isActive ? "active" : ""
+							}
+							onClick={() => setOpen(false)}
+						>
+							HOME
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							to="/about-us"
+							className={({ isActive, isPending }) =>
+								isPending ? "pending" : isActive ? "active" : ""
+							}
+							onClick={() => setOpen(false)}
+						>
+							ABOUT US
+						</NavLink>
+					</li>
+					<li>
+						<a href="/#testimonials" onClick={() => setOpen(false)}>
+							TESTIMONIALS
+						</a>
+					</li>
+					<li>
+						<NavLink
+							to="/contact-us"
+							className={({ isActive, isPending }) =>
+								isPending ? "pending" : isActive ? "active" : ""
+							}
+							onClick={() => setOpen(false)}
+						>
+							CONTACT
+						</NavLink>
+					</li>
+				</ul>
+			</nav>
+
+			<div ref={toggleButtonRef}>
+				<Hamburger
+					toggled={isOpen}
+					toggle={setOpen}
+					direction="right"
+					size={45}
+					color="white"
+				/>
+
+			</div>
+		</header>
+	);
 }
--- src/pages/about-us/AboutUs.tsx
+++ src/pages/about-us/AboutUs.tsx
@@ -1,4 +1,26 @@
+import Header from "../../components/Header";
+import styles from "./aboutUs.module.css";
+
 export default function AboutUs() {
-    return <>
-    </>
+  return (
+    <div>
+      <Header isStaticColor={true} />
+      <div className={styles.about_us_container}>
+        <h1>About Us</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos tempore
+          blanditiis totam eaque perspiciatis esse animi quis ea facere
+          obcaecati, inventore, reiciendis expedita culpa recusandae, optio
+          nulla voluptas cum corrupti quia? Deserunt expedita odit vitae tempora
+          consectetur quam unde beatae quod pariatur accusantium facilis
+          necessitatibus voluptas impedit, id nisi eaque rem tempore quidem
+          quisquam laboriosam neque placeat iste reprehenderit. Aliquid
+          eligendi, fugit maiores deserunt incidunt ea esse nam excepturi porro
+          ipsum minus earum dolorum minima dolore placeat nostrum molestias
+          quisquam sapiente veniam iure quasi reiciendis sit dolorem. Dolor,
+          iure iste. Vel eum maiores, iusto eius laboriosam ut error a veniam!
+        </p>
+      </div>
+    </div>
+  );
 }
